bbacd8e5b66a1774f9ba82d9edc15f47878b23a0
Alex Jilkin <jilkinalex@gmail.com>
2021-01-22 14:21:55 +0200

    Update git ignore

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ sample_rate, data = read("{0}.wav".format(file_name))
 
 # Length of input
 N = int(data.shape[0])
-R = 4.4
+R = 2.2
 
 def odeBackwardEuler(f, x0, u):
     ''' x0 - initial value f - f(x, u) '''
@@ -61,18 +61,18 @@ reduceVolume = np.vectorize(lambda y: y / (8))
 clip = np.vectorize(lambda y: 4.5 if y > 4.5 else -4.5 if y < -4.5 else y)
 uClipped = clip(u)
 
-# xb = odeBackwardEuler(circuit, 0.1, uClipped)
-# xb = reduceVolume(xb)
+xb = odeBackwardEuler(circuit, 0.0, uClipped)
+xb = reduceVolume(xb)
 
-# write("clipped-ode-backward-difference-{0}.wav".format(file_name), sample_rate, xb)
-# print("Created clipped-ode-backward-difference-{0}.wav from {0}".format(file_name))
+write("clipped-ode-backward-difference-{0}-R={1}.wav".format(file_name, R), sample_rate, xb)
+print("Created clipped-ode-backward-difference-{0}.wav from {0}".format(file_name))
 
-xf = odeForwardEuler(circuit, 0.1, uClipped)
+# xf = odeForwardEuler(circuit, 0.1, uClipped)
 
-xf = reduceVolume(xf)
+# xf = reduceVolume(xf)
 
-write("forward-difference-{0}-R={1}.wav".format(file_name, R), sample_rate, xf)
-print("Created forward-difference-{0}.wav from {0}".format(file_name))
+# write("forward-difference-{0}-R={1}.wav".format(file_name, R), sample_rate, xf)
+# print("Created forward-difference-{0}.wav from {0}".format(file_name))
 # plt.figure()
 # plt.plot(y, uClipped, 'b')
 # plt.plot(y, xb, 'r')
